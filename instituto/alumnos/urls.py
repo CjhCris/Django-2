8ddce38cd5d00667f8af950b480329ee204678ff
CjhCris <cmduartesaez@gmail.com>
2024-06-19 14:55:49 -0400

    hola

--- instituto/alumnos/urls.py
+++ instituto/alumnos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('alumnos/index', views.index, name='alumnos/index'),
+    path('crud', views.crud, name='crud'),
 ]
+
+
--- instituto/alumnos/views.py
+++ instituto/alumnos/views.py
@@ -6,3 +6,8 @@ def index(request):
     alumnos = Alumno.objects.all()
     context = {"alumnos":alumnos}
     return render(request, 'index.html', context)
+
+def crud(request):
+    alumnos = Alumno.objects.all()
+    context = {"alumnos":alumnos}
+    return render(request, 'alumnos_list.html', context)
